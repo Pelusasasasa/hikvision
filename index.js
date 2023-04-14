484ea4734b93e922d4d7ed672c08a0359643227e
Pelusa <agustinlorenzatto@gmail.com>
2023-04-13 23:13:20 -0300

    Ya llegue a hacer para traer el buffer

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var request = require('auth-request');
-var options = {
-	scheme: 'http::/Lorenzatto:Lorenzatto12345@',
-	hostname: '192.168.1.101',
-	port: 80,
-	path: '/ISAPI/Streaming/channels/201/picture',
-	method: 'GET',
-    auth: {
-        user: 'Lorenzatto',
-        pass: 'Lorenzatto12345',
-        sendImmediately: false
-      }
-};
-request(options, function (err, result) {
-	if (err) return console.log(err);
-	console.log(result);
+const fs = require('fs');
+
+const {request} = require('urllib');
+const sharp = require('sharp');
+
+request('http://192.168.1.101/ISAPI/Streaming/channels/101/picture',{
+    digestAuth: "Lorenzatto:Lorenzatto12345"
+}).then(async(res)=>{
+    const processedImage = sharp(res.data);
+    const resizeImageBuffer = await processedImage.toBuffer();
+    console.log(resizeImageBuffer)
+
+    fs.writeFileSync('nuevaRuta/Prueba.png',resizeImageBuffer)
 });
